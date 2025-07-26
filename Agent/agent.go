0311a5d8e9269d7dcb0fbda2b3c7ef1984000ed5
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-07-26 21:04:36 +0200

    feat: Agent now Serves a WebServer that provides the system stats

--- Agent/agent.go
+++ Agent/agent.go
@@ -3,28 +3,48 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
-	"time"
 
 	"github.com/thecguygithub/lighthouse/collectors"
 )
 
 func main() {
-	var backend_ip string = os.Getenv("BACKEND_IP")
+	var agent_port string = os.Getenv("AGENT_PORT")
+	var agent_hostname string = os.Getenv("AGENT_HOSTNAME")
+
+	if agent_port == "" {
+		agent_port = "8040"
+	}
+	if agent_hostname == "" {
+		agent_hostname = "localhost"
+	}
 
 	fmt.Println("Starting Lighthouse Agent...")
-	fmt.Println("Trying to connect to backend at:", backend_ip)
 
-	for {
-		stats, err := CollectAll()
-		if err != nil {
-			panic(err)
+	http.HandleFunc("/api/v1/minuteData", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
 		}
 
-		jsonBytes, _ := json.Marshal(stats)
-		fmt.Println(string(jsonBytes))
-		time.Sleep(5 * time.Second)
-	}
+		stats, err := CollectAll()
+		if err != nil {
+			http.Error(w, "Failed to collect stats: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		jsonBytes, err := json.Marshal(stats)
+		if err != nil {
+			http.Error(w, "Failed to marshal stats: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonBytes)
+	})
+
+	fmt.Println("Running WebServer at", agent_hostname+":"+agent_port)
+	http.ListenAndServe(agent_hostname+":"+agent_port, nil)
+
 }
 
 var allCollectors = []collectors.Collector{
